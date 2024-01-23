5a7c5ef6b1fba3b27af98427f46a19c68cbd1905
jjminsuh <minsuh3203@gmail.com>
2024-01-23 15:58:35 +0900

    백준 25501

--- Baekjoon/25501/bj_25501.cpp
+++ Baekjoon/25501/bj_25501.cpp
@@ -1,8 +1,41 @@
-// #include <iostream>
-// #include <cstdio>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
 
-// int main() {
+using namespace std;
 
+int recursionCnt;
 
-//     return 0;
-// }
+int recursion(const char *s, int l, int r){
+
+    recursionCnt ++;
+
+    if(l >= r) return 1;
+    else if(s[l] != s[r]) return 0;
+    else return recursion(s, l+1, r-1);
+}
+
+int isPalindrome(const char *s){
+    return recursion(s, 0, strlen(s)-1);
+}
+
+int main() {
+
+    int t = 0;
+    char s[1000];
+
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++) {
+        scanf("%s", s);
+
+        recursionCnt = 0;
+
+        int isPalindromeResult = isPalindrome(s);
+
+        printf("%d %d\n", isPalindromeResult, recursionCnt);
+    }
+
+    return 0;
+}
